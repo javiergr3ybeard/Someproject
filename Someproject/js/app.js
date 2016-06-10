d6b47797d8a65c36252729ec14e90daaf27d35e5
javiergr3ybeard <jtompkins28@gmail.com>
2016-06-10 08:47:12 -0400

    Add files via upload

---
+++ Someproject/js/app.js
@@ -0,0 +1,27 @@
+var url = 'https://pokeapi.co/api/v2/pokemon/?limit=151'
+
+var template = $('.template')
+  .clone()
+  .removeClass('template')
+  .detach();
+
+function loadPokemon(pokemons) {
+  $.each(pokemons.results, function(i, pokemon) {
+    addPokemon(pokemon);
+  });
+}
+
+function addPokemon(pokemon) {
+  var li = template.clone();
+  li.find('.pokemon-name a')
+    .text(pokemon.name)
+    .attr('href', pokemon.url)
+
+  li.attr('data-id', pokemon.id);
+  $('#pokemonList').append(li);
+
+}
+$.get({
+  url: url,
+  success: loadPokemon
+});
---
+++ Someproject/js/app2.js
@@ -0,0 +1,32 @@
+var url = 'http://pokeapi.co/api/v2/ability/'
+
+$('a[data-remote-pokemon="true"]').on('click', function(e) {
+  e.preventDefault();
+  $.ajax({
+    url: $(this).attr('href'),
+    method: 'get',
+    success: loadPokemon
+  });
+});
+
+function loadPokemon(pokemon) {
+  people.push({
+    firstName: pokemon.name,
+    lastName: pokemon.stats[0].stat.name,
+    secret: pokemon.abilities[0].ability.name
+  });
+}
+
+
+// function getAllPokemon(data) {
+//   $.each(data.results, function(i, pokemon) {
+//     $.ajax({
+//       url: pokemon.url,
+//       method: 'get',
+//       success: loadPokemon
+//     });
+//   });
+//   setTimeout(function() {
+//     listPeople();
+//   }, 5000);
+// }
