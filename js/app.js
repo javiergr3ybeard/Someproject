deeafd8d1cddcbb9d34f9b3b09df0cc36c964945
Matt <mattneal@umail.iu.edu>
2016-06-10 10:10:35 -0400

    Updated master changes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,29 @@
 $(document).foundation();
 
-$(function() {
-    pokedex.init('#pokemonName');
+var url = 'http://40ca7692.ngrok.io/api/v2/pokemon/?limit=151';
+
+var template = $('.template')
+    .clone()
+    .removeClass('template')
+    .detach();
+
+function loadPokemon(pokemons) {
+    $.each(pokemons.results, function(i, pokemon) {
+        addPokemon(pokemon);
+    });
+}
+
+function addPokemon(pokemon) {
+    var li = template.clone();
+    li.find('.pokemon-name a')
+        .text(pokemon.name);
+        //.attr('href', pokemon.url);
+
+    li.attr('data-id', pokemon.id);
+    $('#pokemonList').append(li);
+
+}
+$.get({
+    url: url,
+    success: loadPokemon
 });
-
-var pokedex = {
-        
-
-
-};
-
